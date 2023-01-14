a68f0752dac2ee51a4ddde779cf3c343326277d0
Sooraj-beep <soorajarakkal@gmail.com>
2023-01-14 16:22:55 -0700

    Added draggable marker to observable universe

--- frontend/src/components/LiveMap.js
+++ frontend/src/components/LiveMap.js
@@ -1,5 +1,7 @@
 import { CRS } from 'leaflet';
-import { MapContainer, ImageOverlay, useMap } from 'react-leaflet';
+import { useCallback, useState, useRef, useMemo } from 'react';
+import { MapContainer, ImageOverlay, useMap, useMapEvents, Marker, Popup } from 'react-leaflet';
+
 
 let imageBounds = [
   [0, 0], // padding
@@ -21,15 +23,57 @@ const OverlayImage = () => {
   );
 }
 
+const center = {
+  lat: 51.505,
+  lng: -0.09,
+}
+
+function DraggableMarker() {
+  const [draggable, setDraggable] = useState(false)
+  const [position, setPosition] = useState(center)
+  const markerRef = useRef(null)
+  const eventHandlers = useMemo(
+    () => ({
+      dragend() {
+        const marker = markerRef.current
+        if (marker != null) {
+          setPosition(marker.getLatLng())
+        }
+      },
+    }),
+    [],
+  )
+  const toggleDraggable = useCallback(() => {
+    setDraggable((d) => !d)
+  }, [])
+
+  return (
+    <Marker
+      draggable={draggable}
+      eventHandlers={eventHandlers}
+      position={position}
+      ref={markerRef}>
+      <Popup minWidth={90}>
+        <span onClick={toggleDraggable}>
+          {draggable
+            ? 'Marker is draggable'
+            : 'Click here to make marker draggable'}
+        </span>
+      </Popup>
+    </Marker>
+  )
+}
+
+
 const MapWrapper = () => {
   return (
     <MapContainer
-    bac
       crs={CRS.Simple}
       maxZoom={10}
       minZoom={-4}
       scrollWheelZoom={false}>
 
+      <DraggableMarker />
       <OverlayImage />
 
     </MapContainer>
