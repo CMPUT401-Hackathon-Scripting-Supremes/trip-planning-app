ad8bf5808469796bd75a5c0824454b3f1c167835
Saba Gul <gul@ualberta.ca>
2023-01-14 21:20:19 -0700

    budget calculator

--- frontend/src/TodoApp.js
+++ frontend/src/TodoApp.js
@@ -1,22 +1,26 @@
 import './App.css'
 import React, { useState } from 'react';
+var budget = 0;
 
 function TodoApp() {
-  const date = new Date();
+//   const date = new Date();
 
-  const getTheDate = () => {
-    return ((date.getMonth()-1) < 10 ? ('0'+(date.getMonth()-1)) : (date.getMonth()-1))  + '/' + ((date.getDate() < 10) ? ('0'+ date.getDate()) : date.getDate()) + '/' + date.getFullYear();
-  };
+//   const getTheDate = () => {
+//     return ((date.getMonth()-1) < 10 ? ('0'+(date.getMonth()-1)) : (date.getMonth()-1))  + '/' + ((date.getDate() < 10) ? ('0'+ date.getDate()) : date.getDate()) + '/' + date.getFullYear();
+//   };
 
   const [todoItems, setTodoItems] = React.useState([]);
-  const addTodo = text => {
+  const addTodo = (text, number) => {
     if(text.length <= 117){
-      const newTodoItems = [...todoItems, {text:text, check:false, date: getTheDate()}];
+        budget+= parseInt(number);
+      const newTodoItems = [...todoItems, {text:text, number:number, check:false}];
       setTodoItems(newTodoItems);
     }
   };
   const delItem = index => {
     const newTodoItems = [...todoItems];
+    var item = newTodoItems[index]
+    budget-= parseInt(item.number);
     newTodoItems.splice(index, 1);
     setTodoItems(newTodoItems);
   };
@@ -33,6 +37,10 @@ function TodoApp() {
           <TodoItem index={index} todo={todo} delItem={delItem} done={done}/>
         ))}
       </div>
+      <div className='budget'>
+        <h3 className='text'>Total:</h3>
+        <h3 className='text'>${budget}</h3>
+      </div>
     </div>
   );
 }
@@ -40,28 +48,35 @@ function TodoApp() {
 function TodoItem({todo, delItem, done, index}){
   return (
           <div className = "todo">
-            <div className="delete" onClick={()=>delItem(index)}></div>
-            <div className='todoContent'>
-              <div className='date'>{todo.date}</div>
+            <div className='group1'>
+                <button className='done' onClick={()=>done(index)}>done</button>
+              {/* <div className='date'>{todo.date}</div> */}
               <div className="todoText" style={{textDecoration: todo.check ? "line-through" : ""}}>{todo.text}</div>
-              <button className='done' onClick={()=>done(index)}>done</button>
-            </div>
+              </div>
+              <div className='group2'>
+                <div className='numberText'>${todo.number}</div>
+                <div className="delete" onClick={()=>delItem(index)}></div>
+              </div>
           </div>)
 };
 
 function TodoForm({addTodo}){
   const[input, setInput] = useState("");
+  const[number, setNumber] = useState("");
   const handleSubmit = e => {
     e.preventDefault();
     if(input==="") return;
-    addTodo(input);
+    if(number==="") return;
+    addTodo(input, number);
     setInput("");
+    setNumber("");
   };
 
   return (
   <form onSubmit={handleSubmit}>
-    <input type="text" value={input} onChange={e=>setInput(e.target.value)}></input>
-    <button className="submitButton" type='submit'>submit</button>
+    <input type="text" value={input} placeholder="expense" onChange={e=>setInput(e.target.value)}></input>
+    <input type="number" step="any" placeholder="cost" onChange={e=>setNumber(e.target.value)}></input>
+    <button className="submitButton" type='submit'>Add</button>
   </form>
   );
 };
